4ddc2d29b7ecb163ef72369b68d3f15333e493f6
Gaurav Poudel <074bcsit020.gaurav@scst.edu.np>
2021-12-11 19:01:03 +0545

    new verson

--- app.py
+++ app.py
@@ -85,6 +85,18 @@ def main():
             plt.show()
             st.set_option('deprecation.showPyplotGlobalUse', False)
             st.pyplot()
+        if st.button("Sentiment Analysis"):
+            blob = TextBlob(artical)
+            result_sentiment = blob.sentiment
+            if result_sentiment.polarity > 0:
+                st.markdown("Positive Sentiment ::: :smiley:  ")
+            elif result_sentiment.polarity < 0:
+                st.markdown("Negative Sentiment ::: :angry: ")
+            else:
+                st.markdown("Neutral Sentiment ::: 😐  ")
+            st.success(result_sentiment)
+            
+
             
 
 
@@ -93,45 +105,45 @@ def main():
         st.subheader("A project based on NLP techniques")
     
     
-    elif choice == 'NLP':
-        select_choice = ["Show token and lemma","Show Name Entity","Sentiment Analysis","Text Summarization"]
-        bar_choice = st.selectbox("Select",select_choice)
+   #elif choice == 'NLP':
+        #select_choice = ["Show token and lemma","Show Name Entity","Sentiment Analysis","Text Summarization"]
+        #bar_choice = st.selectbox("Select",select_choice)
 
 
 
-        if bar_choice == "Show token and lemma":
-            text = st.text_area("Enter your text")
+        #if bar_choice == "Show token and lemma":
+            #text = st.text_area("Enter your text")
             
             
 
-            if st.button("Analysis"):
-                nlp_result = text_analyzer(text)
-                st.json(nlp_result)
+            #if st.button("Analysis"):
+                #nlp_result = text_analyzer(text)
+                #st.json(nlp_result)
 
-        if bar_choice == "Show Name Entity":
-            text = st.text_area("enter your text")
+        #if bar_choice == "Show Name Entity":
+            #text = st.text_area("enter your text")
             
             
 
-            if st.button("Analysis"):
-                nlp_result = name_entity(text)
-                st.json(nlp_result)
+            #if st.button("Analysis"):
+                #nlp_result = name_entity(text)
+                #st.json(nlp_result)
 
-        if bar_choice == "Sentiment Analysis":
-            text = st.text_area("enter your text")
+        #if bar_choice == "Sentiment Analysis":
+            #text = st.text_area("enter your text")
             
             
 
-            if st.button("Analysis"):
-                blob = TextBlob(text)
-                result_sentiment = blob.sentiment
-                st.success(result_sentiment)
+            #if st.button("Analysis"):
+                #blob = TextBlob(text)
+                #result_sentiment = blob.sentiment
+                #st.success(result_sentiment)
 
-        if bar_choice == "Text Summarization":
-            text = st.text_area("enter your text")
-            if st.button("Summarize "):
-                summary_result = summarize(text)
-                st.success(summary_result)
+        #if bar_choice == "Text Summarization":
+            #text = st.text_area("enter your text")
+            #if st.button("Summarize "):
+                #summary_result = summarize(text)
+                #st.success(summary_result)
                 
                 
 
